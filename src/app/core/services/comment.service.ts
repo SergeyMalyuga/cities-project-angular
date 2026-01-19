a14fd006e3520fdd2b4b657afe84f6e5ab48c68a
Sergey Malyuga <sergey@malyuga.ru>
2026-01-19 15:59:54 +0300

    Создать comment.service

---
+++ src/app/core/services/comment.service.ts
@@ -0,0 +1,20 @@
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Comment} from '../models/comments';
+import {APIRoute, BASE_URL} from '../constants/const';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommentService {
+  private http = inject(HttpClient);
+
+  getComments(offerId: string): Observable<Comment> {
+    return this.http.get<Comment>(`${BASE_URL}/${APIRoute.COMMENTS}/${offerId}`)
+  }
+
+  postComment(rating: number, comment: string): Observable<Comment> {
+    return this.http.post<Comment>(`${BASE_URL}/${APIRoute.COMMENTS}/${rating}/`, {rating, comment})
+  }
+}
